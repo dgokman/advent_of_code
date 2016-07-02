5cfcf6e8dac012f71be0a5716e7627ec35b74612
Dennis Gokman <dennis.gokman@gmail.com>
2016-07-02 17:08:34 -0400

    solved first AOC problem

---
+++ day_1/day_1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "io/ioutil"
+  "fmt"
+)
+
+func main() {
+  contents, _ := ioutil.ReadFile("day_1/day_1.txt")
+  floor := 0
+  for _, val := range contents {
+    if string(val) == ")" {
+      floor--
+    } else if string(val) == "(" {
+      floor++
+    }
+  }
+  fmt.Println(floor)
+}
